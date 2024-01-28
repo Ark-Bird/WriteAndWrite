c935da2562d470d0d658d06ace4c25719671fad5
aqua <axis-order@protonmail.com>
2024-01-29 06:41:52 +0900

    フラグ管理で早期リターンを実装

--- full_mode.py
+++ full_mode.py
@@ -1,6 +1,7 @@
 class FullMode:
     def __init__(self, root):
         self.root = root
+        self.c_mode_flag = False
 
     def start_c_mode(self) -> None:
         """
@@ -8,6 +9,9 @@ class FullMode:
         集中モード開始（フルスクリーンになる）
         返り値無し
         """
+        if self.c_mode_flag:
+            return
+        self.c_mode_flag = True
         self.root.attributes("-fullscreen", True)
         return
 
@@ -17,6 +21,9 @@ class FullMode:
         集中モード終了（フルスクリーンは解除されるがウィンドウからフォーカスが外れない場合があるので注意
         返り値無し
         """
+        if not self.c_mode_flag:
+            return
+        self.c_mode_flag = False
         self.root.attributes("-fullscreen", False)
         self.root.geometry("640x640")
         return
