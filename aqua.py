38abb052bf30b97955f221bde187ca2460f1927c
aqua <axis-order@protonmail.com>
2024-02-03 02:42:30 +0900

    初期化時に保存フラグがTrueだったのを変更

--- aqua.py
+++ aqua.py
@@ -68,7 +68,7 @@ class WillBeAuthor:
         self.is_changed: bool = False
         self.clipped_text: str = ""
         self.pasting_text: str = ""
-        self.is_save: bool = True
+        self.is_save: bool = False
         self.is_exit: bool = False
         self.is_autosave_flag: bool = False
         self.dark_mode: bool = False
