7b17dc14385e7b18103558ded0e1309c93d20c01
aqua <axis-order@protonmail.com>
2024-02-19 04:18:41 +0900

    docstringの追加

--- inmemory_module/ram_memo.py
+++ inmemory_module/ram_memo.py
@@ -1,10 +1,23 @@
 class RamMemo:
     def __init__(self):
+        """
+        フィールドの初期化
+        """
         self.memo: str = "I am amnesia"
 
     def new_memo(self, memo: str) -> None:
+        """
+        実行中に変数memoに文字列を保持
+        スクリプト終了時に変数は破棄される
+        :param memo:
+        :return:
+        """
         self.memo = memo
         return
 
     def remember(self) -> str:
+        """
+        変数memoを返す関数
+        :return:
+        """
         return self.memo
