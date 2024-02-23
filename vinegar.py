d6e677ea6da45138534702c621f09439f74ec841
aqua <axis-order@protonmail.com>
2024-02-24 02:16:09 +0900

    型ヒントの追加

--- vinegar.py
+++ vinegar.py
@@ -13,8 +13,8 @@ class Vinegar:
         テキストをシリアライズして保存
         :return:
         """
-        all_text = self.page.get("0.0", "end")
-        pkl = tk.filedialog.asksaveasfilename()
+        all_text: str = self.page.get("0.0", "end")
+        pkl: str = tk.filedialog.asksaveasfilename()
         pkl = pkl + ".pkl"
         with open(pkl, "wb") as f:
             pickle.dump(all_text, f)
@@ -30,7 +30,7 @@ class Vinegar:
         if pkl == "":
             return
         with open(pkl, "rb") as f:
-            deserialized_text = pickle.load(f)
+            deserialized_text: str = pickle.load(f)
         self.page.delete("0.0", "end")
         self.page.insert("insert", deserialized_text[:-1])
         return
