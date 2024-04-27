7c450f99fdfabfbf8f03d3c632b4591d4bc5475e
aqua <axis-order@protonmail.com>
2024-04-28 00:08:23 +0900

    アーカイブの失敗時にメッセージボックスを表示

--- archive.py
+++ archive.py
@@ -1,10 +1,12 @@
 from shutil import make_archive
 
 import extend_exception
+import independent_method
 
 if __name__ == '__main__':
     try:
         make_archive('aqua', format='zip', root_dir='dist')
     except Exception:
         print('zipファイルの作成に失敗しました')
+        independent_method.fix_this_later()
         raise extend_exception.FatalError
