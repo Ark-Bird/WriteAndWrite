047cb187d61e24c5d7a4137d1f5d77e2d3fcc06c
aqua <axis-order@protonmail.com>
2024-05-07 19:03:57 +0900

    バージョン名を変更

--- app_name.py
+++ app_name.py
@@ -5,6 +5,7 @@ class AppName:
     def __init__(self):
         """
         アプリ名を定数として設定
+        タイトルバーに表示されるアプリ名（仮）
         """
         self._APP_CODE_NAME: const.Const = const.Const("""雛芥子""")
 
