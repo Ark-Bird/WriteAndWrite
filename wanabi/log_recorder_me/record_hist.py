2b1e3d6701e31c5b1ae0c3a41007b8edb290bc25
skylake <skylake.author@gmail.com>
2024-10-25 13:45:53 +0900

    ログの書式を修正

--- wanabi/log_recorder_me/record_hist.py
+++ wanabi/log_recorder_me/record_hist.py
@@ -7,7 +7,7 @@ class RecordHist:
 
     def write_log(self, message: str) -> None:
         now_time = datetime.datetime.now()
-        err_message = f"{now_time.year}:{now_time.month}:{now_time.day}:{now_time.hour}:{now_time.minute}:{now_time.second}:{message}"
+        err_message = f"{now_time.year}/{now_time.month}/{now_time.day}-:{now_time.hour}:{now_time.minute}:{now_time.second}:{message}"
         with open(self.filename, mode="a+", encoding="utf-8") as f:
             f.write(err_message + "\n")
 
