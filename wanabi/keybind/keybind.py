7d54653cd3ca828bffc3ce811e68d89f9b68be67
omoikane <axis-order@protonmail.com>
2024-11-21 09:20:19 +0900

    プラットフォームの違いを急襲できるように変更

--- wanabi/keybind/keybind.py
+++ wanabi/keybind/keybind.py
@@ -1,49 +1,69 @@
 import ctypes
 import tkinter.messagebox
+
 class KeyBindMode:
     def __init__(self, author):
         self.author = author
         self.vi_insert_mode = None
         self.vi_command_mode = None
         self.is_windows = False
+        self.is_windows_flag = False
+
+
+    def check_windows(self):
         try:
             with open("conf/platforms.txt", "r", encoding=self.author.code) as bindtype:
-                self.is_windows = bindtype.read()
-            if self.is_windows == "Windows":
+                self.is_windows_flag = bindtype.read()
+            if self.is_windows_flag == "Windows":
                 self.is_windows = True
+                return self.is_windows
+            else:
+                self.is_windows = False
+                return self.is_windows
         except FileNotFoundError:
             pfask = tkinter.messagebox.askyesno("win or No win", "Windowsを使用していますか？")
             if pfask:
                 with open("conf/platforms.txt", "w", encoding=self.author.code) as bindtype:
                     bindtype.write("Windows")
                     self.is_windows = True
+                    return self.is_windows
             else:
                 with open("conf/platforms.txt", "w", encoding=self.author.code) as bindtype:
-                    bindtype.write("AnothorOS")
+                    bindtype.write("AnotherOS")
                     self.is_windows = False
+                    tkinter.messagebox.showwarning("このアプリを再起動してください", "Windows以外で使用する場合、初期設定のために再起動をしてください")
+                    self.author.exit_as_save()
+                    return self.is_windows
         except:
             raise Exception
 
-
     def cursor_move_forward(self, event=None) -> str | None:
+        if not self.check_windows():
+            return
         if self.if_enable_ime():
             return
         self.author.page.mark_set("insert", "insert+1c")
         return "break"
 
     def cursor_move_next_line(self, event=None) -> str | None:
+        if not self.check_windows():
+            return
         if self.if_enable_ime():
             return
         self.author.page.mark_set("insert", "insert+1lines")
         return "break"
 
     def cursor_move_prev_line(self, event=None) -> str | None:
+        if not self.check_windows():
+            return
         if self.if_enable_ime():
             return
         self.author.page.mark_set("insert", "insert-1lines")
         return "break"
 
     def cursor_move_backward(self, event=None) -> str | None:
+        if not self.check_windows():
+            return
         if self.if_enable_ime():
             return
         self.author.page.mark_set("insert", "insert-1c")
@@ -53,7 +73,6 @@ class KeyBindMode:
         pass
 
     def insert_mode(self, event=None) -> str:
-        self.vi_insert_mode = True
         self.vi_insert_mode = ViInsertMode(self.author)
         self.vi_insert_mode.edit_key_bind()
         return "break"
@@ -87,8 +106,6 @@ class KeyBindMode:
         self.vi_command_mode.edit_key_bind()
 
     def if_enable_ime(self) -> bool | None:
-        if not self.is_windows:
-            return
         wnd = ctypes.WinDLL(name="user32")
         himc = ctypes.WinDLL(name="imm32")
         ime_handle = wnd.GetForegroundWindow()
@@ -104,7 +121,7 @@ class KeyBindMode:
 class ViCommandMode(KeyBindMode):
     def edit_key_bind(self) -> None:
         self.disable_emacs_mode()
-        if not self.is_windows:
+        if not self.check_windows():
             self.insert_mode()
             return
         self.author.page.bind("<h>", self.cursor_move_backward)
@@ -117,6 +134,7 @@ class ViCommandMode(KeyBindMode):
 
 class ViInsertMode(KeyBindMode):
     def edit_key_bind(self) -> None:
+        self.disable_vi_mode()
         self.author.page.bind("<i>", self.insert_ignor)
         self.author.page.bind("<Escape>", self.command_mode)
         self.author.page.bind("<Control-[>", self.command_mode)
