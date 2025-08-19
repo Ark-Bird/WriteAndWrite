6f90c7dbd486bcf2f93653c30583dd79f772d9d3
skylake <axis-order@protonmail.com>
2025-08-19 19:48:29 +0900

    変数名のtypoを修正

--- wanabi/version.py
+++ wanabi/version.py
@@ -49,7 +49,7 @@ class ShowInfo:
     """)
         # バージョン
         self._VERSION: const.Const = const.Const("""
-        ver2.9.0_code:/Archive/
+        ver2.9.1_code:/Archive/
         """)
 
         # テーマ書式
