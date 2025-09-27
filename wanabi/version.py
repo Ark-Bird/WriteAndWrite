5897cc13270063909bed146f2ca1d81fe9fb597a
skylake <axis-order@protonmail.com>
2025-09-28 07:31:04 +0900

    一時ファイルの保存を試験的にスレッド化出来るよう変更

--- wanabi/version.py
+++ wanabi/version.py
@@ -49,7 +49,7 @@ class ShowInfo:
     """)
         # バージョン
         self._VERSION: const.Const = const.Const("""
-        ver2.9.10_code:/Archive/
+        ver2.9.11_code:/Archive/
         """)
 
         # テーマ書式
