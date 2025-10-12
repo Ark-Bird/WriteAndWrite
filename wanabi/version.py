14cd489c110e653e427363f1299838df71e11415
skylake <axis-order@protonmail.com>
2025-10-12 19:52:04 +0900

    初期化処理の変更と、累積的なバグ修正

--- wanabi/version.py
+++ wanabi/version.py
@@ -49,7 +49,7 @@ class ShowInfo:
     """)
         # バージョン
         self._VERSION: const.Const = const.Const("""
-        ver2.9.12_code:/Archive/
+        ver2.10.0_code:/Hope Is Good/
         """)
 
         # テーマ書式
